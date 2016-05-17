be3ed31f7c1a5c139fc659c5225c3cc9cf6eb190
spkal <spk@seapowet.org>
2016-05-16 18:50:49 -0700

    Read from github and clean up.

--- clean.R
+++ clean.R
@@ -3,3 +3,13 @@ indx <- with(elevators, which(LONGITUDE < -75 & LATITUDE < 40.4))
 if(length(indx) > 0) {
     elevators <- elevators[-indx, ]
 }
+countNA <- sapply(elevators, function(x) sum(is.na(x)))
+# Drop TEST, DUMMY observations:
+indxTESTDUMMY <- with(elevators,
+    which(HOUSE_NUMBER == "TEST" | STREET_NAME == "DUMMY RECORD"))
+elevators <- elevators[-indxTESTDUMMY, ]
+# Trim white space
+indxChar <- which(sapply(elevators, is.character))
+for(i in indxChar) {
+	elevators[, i] <- trimws(elevators[, i])
+}
--- read.R
+++ read.R
@@ -1,3 +1,14 @@
+# Using local elevators.csv file:
 p <- pipe("sed -e 1d -e 's/DV_FLOOR_TO,,LATITUDE/DV_FLOOR_TO,XJUNK,LATITUDE/' elevators.csv")
 elevators <- read.csv(p, stringsAsFactors = FALSE)
 elevators$XJUNK <- NULL
+
+# Reading data from github:
+DATAURL <- "https://github.com/datanews/elevators/raw/master/elevators.csv"
+elevators <- read.csv(DATAURL, stringsAsFactors = FALSE, skip = 1)
+elevators$X <- NULL
+# Set dates
+elevators$DV_LASTPER_INSP_DATE <-
+    as.Date(as.character(elevators$DV_LASTPER_INSP_DATE), format="%Y%m%d")
+elevators$DV_STATUS_DATE <-
+    as.Date(as.character(elevators$DV_STATUS_DATE), format="%Y%m%d")
